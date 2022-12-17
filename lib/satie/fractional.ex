865f68ef4a70aa901d78ee8a478257e5954fde70
Michael Berkowitz <michael.berkowitz@gmail.com>
2022-12-17 15:07:01 -0500

    Implement correctly resolving arithmetic functions for fraction-like structs
    Implement correctly resolving arithmetic functions for
    
    * Duration
    * Multiplier
    * Offset
    
    Move shared functionality to Fractional module
    
    * `reducee`
    * `correct_polarity`
    * `__init__`

---
+++ lib/satie/fractional.ex
@@ -0,0 +1,29 @@
+defmodule Satie.Fractional do
+  @moduledoc """
+  Implements shared functionality for fractional-types
+
+    * `Satie.Duration`
+    * `Satie.Multiplier`
+    * `Satie.Offset`
+    * `Satie.Fraction`
+
+  """
+
+  def to_tuple(%{numerator: n, denominator: d}), do: {n, d}
+
+  def __init__({n, d}, mod) do
+    {n, d}
+    |> reduce()
+    |> then(fn {n, d} -> struct(mod, %{numerator: n, denominator: d}) end)
+  end
+
+  def reduce({a, b}) do
+    with g <- Integer.gcd(a, b) do
+      {round(a / g), round(b / g)}
+      |> correct_polarity()
+    end
+  end
+
+  defp correct_polarity({a, b}) when b < 0, do: {a * -1, b * -1}
+  defp correct_polarity({a, b}), do: {a, b}
+end
--- test/satie/multiplier_test.exs
+++ test/satie/multiplier_test.exs
@@ -1,7 +1,7 @@
 defmodule Satie.MultiplierTest do
   use ExUnit.Case, async: true
 
-  alias Satie.Multiplier
+  alias Satie.{Duration, Multiplier, Offset}
 
   describe inspect(&Multiplier.new/2) do
     test "takes integral numerator and denominator" do
@@ -11,10 +11,10 @@ defmodule Satie.MultiplierTest do
              }
     end
 
-    test "doesnt' reduce fractions" do
+    test "reduces fractions" do
       assert Multiplier.new(4, 6) == %Multiplier{
-               numerator: 4,
-               denominator: 6
+               numerator: 2,
+               denominator: 3
              }
     end
 
@@ -24,6 +24,45 @@ defmodule Satie.MultiplierTest do
 
         assert Multiplier.new(3, invalid) == {:error, :multiplier_new, {3, invalid}}
       end
+    end
+  end
+
+  describe "arithmetic" do
+    test "against a duration" do
+      m = Multiplier.new(1, 5)
+      d = Duration.new(1, 8)
+
+      assert is_struct(Multiplier.add(m, d), Multiplier)
+      assert is_struct(Multiplier.subtract(m, d), Multiplier)
+      assert is_struct(Multiplier.multiply(m, d), Duration)
+      assert is_struct(Multiplier.divide(m, d), Multiplier)
+    end
+
+    test "against an integer" do
+      m = Multiplier.new(1, 5)
+
+      assert is_struct(Multiplier.multiply(m, 2), Multiplier)
+      assert is_struct(Multiplier.divide(m, 2), Multiplier)
+    end
+
+    test "against another multiplier" do
+      m1 = Multiplier.new(1, 5)
+      m2 = Multiplier.new(1, 8)
+
+      assert is_struct(Multiplier.add(m1, m2), Multiplier)
+      assert is_struct(Multiplier.subtract(m1, m2), Multiplier)
+      assert is_struct(Multiplier.multiply(m1, m2), Multiplier)
+      assert is_struct(Multiplier.divide(m1, m2), Multiplier)
+    end
+
+    test "against an offset" do
+      m = Multiplier.new(1, 5)
+      o = Offset.new(1, 8)
+
+      assert is_struct(Multiplier.add(m, o), Multiplier)
+      assert is_struct(Multiplier.subtract(m, o), Multiplier)
+      assert is_struct(Multiplier.multiply(m, o), Multiplier)
+      assert is_struct(Multiplier.divide(m, o), Multiplier)
     end
   end
 end
